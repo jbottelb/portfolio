81774fbc65ee0c1cd7e210e76a66353ea2d682b8
jbottelb <jbottelb@nd.edu>
2021-09-01 21:04:21 -0400

    styling and organization

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,12 +1,22 @@
 import React from "react";
+import photo from "../img/standing.JPEG"
 
 // The home page.
 const Home = () => {
   return (
-      <div class="standard">
-     <h1>
-        Welcome to my Portfolio Website
-     </h1>
+  <div class="standard">
+  <div class="flex-container">
+      <img class="standing" src={photo} alt=""/>
+      <div class="profile">
+         <h1>
+            Josh Bottelberghe
+         </h1>
+         <p>
+            I am a current senior Computer Science Major at the University of
+            Notre Dame, currently seeking job oppertunities.  
+         </p>
+      </div>
+    </div>
     </div>
   );
 };
