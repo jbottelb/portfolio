852f7c8270ce61fee5f70f53c51f3c3f4f6b57d5
Josh Bottelberghe <62488350+jbottelb@users.noreply.github.com>
2021-09-06 23:41:02 -0400

    added work experiance

--- src/Components/home/interests.js
+++ src/Components/home/interests.js
@@ -19,6 +19,52 @@ const Interests = () => {
             I have been playing electric guitar for 5 years. I have
             since picked up bass, piano and ukelele.
         </p>
+        <h2 class="title-interests">Work Experiance</h2>
+        <h3>
+            Software Development Intern - RJ Reliance
+        </h3>
+        <p>
+            Developed RPA 
+            production software using Help Systems Automate and python
+        </p>
+        <h3>
+            Teaching Assitant - Data Structures
+        </h3>
+        <p>
+            TA for data strucutres class. Lead a group of students in 
+            industry coding problems with companies such as Microsoft.
+            Also held office hours and attended labs to help students. 
+        </p>
+        <h3>
+            Teaching Assitant - Programming Challenges
+        </h3>
+        <p>
+            TA for programming challenge course where students learn how to 
+            solve interview type programming questions and put to use 
+            the skills learned in Data Structures and Algorithms. 
+        </p>
+        <h3>
+            Teaching Assitant - Fundementals of Computing
+        </h3>
+        <p>
+            Assisted teaching a fudemental computer science course in C
+        </p>
+        <h3>
+            ND AVT Technition 
+        </h3>
+        <p>
+            Maintained school hardware for remote teaching during the
+            pandemic. 
+        </p>
+        <h3>
+            Stanford Code in Place Section Leader
+        </h3>
+        <p>
+            Though not technically a job, I lead a very diverse group of 
+            students through a coding camp offered by Stanford University. 
+            I lead tutorial sessions, assisting the learning of python and 
+            basic coding concepts. 
+        </p>
       </div>
   )
 }
